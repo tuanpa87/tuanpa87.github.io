cad889b3587aac0eb13cc9f76b9f5766f71d3bd9
tuanpa87 <tuanpa87@users.noreply.github.com>
2017-11-14 03:52:23 +0700

    ok

--- project-web/js/script.js
+++ project-web/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -16,9 +15,6 @@ $(document).ready(function(){
 	});
 
 
-=======
-$(document).ready(function(){
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 	$('#product-slider').owlCarousel({
 		loop: true,
 		autoplay: true,
@@ -60,11 +56,7 @@ $(document).ready(function(){
 			425:{
 				items:1
 			},
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 			768:{
 				items:2
 			},
@@ -74,10 +66,7 @@ $(document).ready(function(){
 		}
 	})
 
-<<<<<<< HEAD
 
-=======
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 	$('#home-newproduct-slider').owlCarousel({
 		loop: true,
 		autoplay: true,
@@ -94,11 +83,7 @@ $(document).ready(function(){
 			425:{
 				items:2
 			},
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 			768:{
 				items:3
 			},
@@ -110,10 +95,7 @@ $(document).ready(function(){
 		}
 	})
 
-<<<<<<< HEAD
 
-=======
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 	$('#promotion-slider').owlCarousel({
 		loop: true,
 		autoplay: true,
@@ -130,11 +112,7 @@ $(document).ready(function(){
 			425:{
 				items:2
 			},
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 			768:{
 				items:3
 			},
@@ -147,7 +125,6 @@ $(document).ready(function(){
 	})
 
 
-<<<<<<< HEAD
 	var eventFired = 0;
 
 	if ($(window).width() < 768) {
@@ -181,8 +158,6 @@ $(document).ready(function(){
 	accordionOn: ['xs'] // xs, sm, md, lg
 	});
 
-=======
->>>>>>> b0e743949ff3cb891fadf58cc8e279530295309c
 
 });
 
