4296f832791c7667a317b58021b51e0134adfa50
tuanpa87 <tuanpa87@users.noreply.github.com>
2017-11-14 05:07:33 +0700

    OK

--- project-web/js/script.js
+++ project-web/js/script.js
@@ -1,4 +1,21 @@
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backTop").style.display = "block";
+    } else {
+        document.getElementById("backTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
 
 
 $(document).ready(function(){
@@ -6,12 +23,12 @@ $(document).ready(function(){
 	var hash = document.location.hash;
 	var prefix = "tab_";
 	if (hash) {
-	    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+		$('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
 	} 
 
 	// Change hash for page-reload
 	$('.nav-tabs a').on('shown.bs.tab', function (e) {
-	    window.location.hash = e.target.hash.replace("#", "#" + prefix);
+		window.location.hash = e.target.hash.replace("#", "#" + prefix);
 	});
 
 
@@ -156,10 +173,11 @@ $(document).ready(function(){
 
 	$('.responsive-tabs').responsiveTabs({
 	accordionOn: ['xs'] // xs, sm, md, lg
-	});
+});
 
 
 });
+
 
 
 
