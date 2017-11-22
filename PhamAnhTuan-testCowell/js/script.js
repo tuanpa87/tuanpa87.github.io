ee15b2fcdbcc3ba9607d670a85d2ea9100e71db0
tuanpa <tuan.pa.1987@gmail.com>
2017-11-22 15:17:15 +0700

    ok

--- PhamAnhTuan-testCowell/js/script.js
+++ PhamAnhTuan-testCowell/js/script.js
@@ -17,7 +17,33 @@ $(document).ready(function(){
 		autoplayTimeout: 3000,
 	});
 
+
+
+	var countDownDate = new Date("Nov 24, 2017 18:00:00").getTime();
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+	// Get todays date and time
+	var now = new Date().getTime();
+
+	// Find the distance between now an the count down date
+	var distance = countDownDate - now;
+
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	$('#days').text(days);
+	$('#hours').text(hours);
+	$('#minutes').text(minutes);
+	$('#second').text(seconds);
+
+	}, 1000);
 });
+
+
 
 
 
