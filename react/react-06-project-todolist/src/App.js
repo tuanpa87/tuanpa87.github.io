e1fdc620e2b780bedc41b6fa7ecdbe35cbbe437f
Tuanpa <tuan.pa.1987@gmail.com>
2018-02-12 14:55:23 +0700

    react todolist update components

--- react/react-06-project-todolist/src/App.js
+++ react/react-06-project-todolist/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './components/TaskForm'
+import Control from './components/Control'
+import TaskList from './components/TaskList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="text-center">
+            <h1>Quản Lý Công Việc</h1>
+            <hr />
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              <TaskForm/>
+            </div>
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <button type="button" className="btn btn-primary">
+                <span className="fa fa-plus mr-5"></span>Thêm Công Việc
+              </button>            
+              <Control/>
+              <div className="row mt-15">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <TaskList/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
