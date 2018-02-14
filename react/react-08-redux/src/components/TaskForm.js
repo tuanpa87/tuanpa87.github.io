3c6df692728c204dfecaf3592c81f7d7565e8d9e
tuanpa87 <tuanpa87@users.noreply.github.com>
2018-02-14 17:36:28 +0700

    react redux

---
+++ react/react-08-redux/src/components/TaskForm.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+
+class TaskForm extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = { //state này để lưu trữ giá trị của form 
+            id: '',
+            name: '',
+            status: false,
+        }
+    }
+
+    //life cycle: nhận lại prop task = state.taskEditing từ ngoài component app
+    componentWillMount() {
+        console.log('componentWillMount: goi duoc 1 lan dau')
+        if(this.props.task) {
+            this.setState ({ //set lại state của component này (TaskForm)
+                id: this.props.task.id,
+                name: this.props.task.name,
+                status: this.props.task.status
+            })
+        }  //sửa cũng qua function onChange -> onSubmit 
+    }
+
+    //life cycle: thay đổi state khi thêm <=> sửa
+    componentWillReceiveProps(nextProps) {
+        console.log('componentWillReceiveProps:')
+        console.log(nextProps) //nextProps: nhận lại prop task = state.taskEditing  khi thay đổi thêm <=> sửa  
+        if(nextProps && nextProps.task) { //khi sửa thì có state.taskEditing tức là cũng có nextProps.task 
+            console.log('thêm => sửa')
+            this.setState ({
+                id: nextProps.task.id,
+                name: nextProps.task.name,
+                status: nextProps.task.status
+            })
+        } else if (!nextProps.task) { //khi thêm thì state.taskEditing = null tức là nextProps.task = null luôn
+            console.log('sửa => thêm') 
+            this.setState ({
+                id: '',
+                name: '',
+                status: false,
+            })
+        }
+    }
+
+    onCloseForm = () => {
+        this.props.onCloseForm(); 
+    }
+
+    onChange = (event) => { 
+        var target = event.target;
+        var name = target.name;
+        var value = target.value
+        if (name === 'status') {
+            value = target.value === 'true' ? true : false
+        };
+        this.setState({
+            [name]: value //set lại state Form
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+        this.props.onSubmit(this.state) //chay function ben ngoai app.js (truyền vào giá trị của state lưu giá trị form ở trên)
+
+        //Submit xong phai clear va close form
+        this.onClear();
+        this.onCloseForm();
+    }
+
+    //chức năng hủy bỏ khi nhập form
+    onClear = () => {
+        this.setState({
+            name: '',
+            status: false,
+        })
+    }
+
+    render() {
+        var {id} = this.state
+        return (
+            <div className="panel panel-warning">
+                <div className="panel-heading">
+                    <h3 className="panel-title">
+                        {id!=='' ? 'Cập Nhật Công Việc' : 'Thêm Công Việc'}
+                        <span
+                            className="fa fa-times-circle text-right"
+                            onClick={this.onCloseForm}
+                        ></span>
+                    </h3>
+                </div>
+                <div className="panel-body">
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label>Tên :</label>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                name="name"
+                                value={this.state.name} 
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <label>Trạng Thái :</label>
+                        <select 
+                            className="form-control" 
+                            required="required" 
+                            name="status"
+                            value={this.state.status} 
+                            onChange={this.onChange}
+                        >
+                            <option value={true}>Kích Hoạt</option>
+                            <option value={false}>Ẩn</option>
+                        </select>
+                        <br />
+                        <div className="text-center">
+                            <button type="submit" className="btn btn-warning">Thêm</button>&nbsp;
+                            <button type="submit" className="btn btn-danger" onClick={this.OnClear} >Hủy Bỏ</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TaskForm;
