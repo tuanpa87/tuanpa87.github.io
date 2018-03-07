56428d29c0ede3e68caf5446cabad7750c2b24e4
Tuanpa <tuan.pa.1987@gmail.com>
2018-03-07 15:08:23 +0700

    React : react-router

---
+++ react/react-11-react-router/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App"> 
+
+          {/* Menu       */}
+          <nav className="navbar navbar-inverse">
+            <ul className="nav navbar-nav">
+              <li className="active">
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+              <Link to="/about">About</Link>
+              </li>
+              <li>
+              <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Content        */}
+          <Route path="/" exact component={Home} /> 
+          <Route path="/about" component={About} /> 
+          <Route path="/contact" component={Contact} /> 
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
