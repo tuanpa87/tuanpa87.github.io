61e3e09c9a492989995190973861629c62f9d079
Tuanpa <tuan.pa.1987@gmail.com>
2018-06-06 11:03:44 +0700

    node in progress

--- nodeJS/course/file-writing.js
+++ nodeJS/course/file-writing.js
@@ -6,6 +6,5 @@ fs.writeFile('./modules/data.html', 'hello this file has just been created \n', 
 
 fs.appendFile('./modules/data.html', 'extra data apppended to this file', 'utf8', (err, content) => {
     if(err) return err;
-
     console.log('this file has been saved');
 })
