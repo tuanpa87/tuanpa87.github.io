bd25217204962093184d2dec5bee8b6a066867d0
Tuanpa <tuan.pa.1987@gmail.com>
2018-06-08 15:24:31 +0700

    update

--- nodeJS/course/websocket-client.js
+++ nodeJS/course/websocket-client.js
@@ -1,1 +1,36 @@
 const WS = new WebSocket('ws://localhost:3233');
+
+function displayTitle(title) {
+    document.querySelector('h1').innerHTML = title;
+}
+
+
+function displayMessages (message) {
+    let h2 = document.createElement('h2');
+    h2.innerText = message;
+    document.querySelector('div.messages').appendChild(h2);
+}
+
+
+WS.onopen = () => {
+    console.log('connection open');
+    displayTitle('connected');
+}
+
+
+WS.onclose = () => {
+    console.log('connection close')
+    displayTitle('disconnected');
+}
+
+
+WS.onmessage = (payload) => {
+    console.log(payload.data);
+    displayMessages(payload.data);
+};
+
+
+document.forms[0].onsubmit = () => {
+    let input = document.getElementById('message');
+    WS.send(input.value);
+};
--- nodeJS/course/websocket-server.js
+++ nodeJS/course/websocket-server.js
@@ -3,6 +3,18 @@ const WSS = new WebSocketServer({port: 3233});
 
 
 WSS.on('connection', (ws) => {
+
+    ws.on('message', (message) => {
+        if(message === 'close') {
+                ws.close();
+        } else {
+            WSS.clients.forEach((client)=> {
+                client.send(message);
+            })
+        }
+
+        console.log(message);
+    })
     console.log('connected')
 
 })
