cdd515b429bddf9613fbf9c4cf92bc86ea086b34
Tuanpa <tuan.pa.1987@gmail.com>
2018-06-08 18:12:55 +0700

    no message

--- nodeJS/course/mongodb/app.js
+++ nodeJS/course/mongodb/app.js
@@ -9,11 +9,32 @@
 // })
 //})
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/animals');
-mongoose.connection.once('open', () => {
+// mongoose.connect('mongodb://localhost:27017/animals');
+// mongoose.connection.once('open', () => {
+//   console.log('connected');
+// }).on('error', (err)=> {
+//   console.log('could not connected', err)
+// })
+
+const {MongoClient, ObjectId} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+  if (err) throw err;
+  const db = client.db('animals');
+  const object = ObjectId();
+  console.log(object);
   console.log('connected');
-}).on('error', (err)=> {
-  console.log('could not connected', err)
+
+  db.collection('mammals').insertOne({
+    name: 'cat',
+    legs: 4
+
+  }, (err, result) => {
+    if (err) return console.log(err);
+    console.log('inserted');
+
+  })
 })
+
+
