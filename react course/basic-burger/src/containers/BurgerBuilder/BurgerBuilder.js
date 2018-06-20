0dad527dffd123ca6caa49bbd36a531d36fa68c8
Tuanpa <tuan.pa.1987@gmail.com>
2018-06-20 17:54:34 +0700

    update

---
+++ react course/basic-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.2,
+  bacon: 1.5,
+  cheese: 0.5,
+  meat: 1.1
+};
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updateIngrediens = {
+      ...this.state.ingredients
+    };
+    updateIngrediens[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({
+      ingredients: updateIngrediens,
+      totalPrice: newPrice
+    });
+  };
+
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) return;
+
+    const updatedCount = oldCount - 1;
+    const updateIngrediens = {
+      ...this.state.ingredients
+    };
+    updateIngrediens[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({
+      ingredients: updateIngrediens,
+      totalPrice: newPrice
+    });
+  };
+
+  render() {
+    const disalbeInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disalbeInfo) {
+      disalbeInfo[key] = disalbeInfo <= 0 
+    }
+
+    console.log(disalbeInfo);
+
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+          addIngridient={this.addIngredientHandler}
+          removeIngridient={this.removeIngredientHandler}
+          disabled={disalbeInfo}
+        />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
