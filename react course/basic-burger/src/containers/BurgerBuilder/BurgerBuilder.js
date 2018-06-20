aee7244876bf0666fac0a2070f49440e54a70909
Tuanpa <tuan.pa.1987@gmail.com>
2018-06-20 17:58:47 +0700

    fix Aux

--- react course/basic-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react course/basic-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/reactAux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
