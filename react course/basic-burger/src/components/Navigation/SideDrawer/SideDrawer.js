9ec4230246e91632c3d84f98abc4813f328ade00
tuanpa87 <tuanpa87@users.noreply.github.com>
2018-06-21 22:46:50 +0700

    no message

--- react course/basic-burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react course/basic-burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,24 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import classes from "./SideDrawer.css";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import BackDrops from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/reactAux";
 
 const sideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) attachedClasses = [classes.SideDrawer, classes.Open];
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo //height="10%"
-        />
+    <Aux>
+      <BackDrops show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo //height="10%"
+          />
+        </div>
+        <NavigationItems />
       </div>
-      <NavigationItems />
-    </div>
+    </Aux>
   );
 };
 
