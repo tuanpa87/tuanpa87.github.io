a94debe94d372aad7c80388907dc4399d13f2677
tuanpa87 <tuanpa87@users.noreply.github.com>
2018-07-05 23:39:15 +0700

    update

--- react course/redux-01-start/src/index.js
+++ react course/redux-01-start/src/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reducer from "./store/reducer";
 import registerServiceWorker from "./registerServiceWorker";
 
+/* adding middleware */
+// const logger = store => {
+//   return next => {
+//     return action => {
+//       console.log("[Middleware Dispatching]", action);
+//       const result = next(action);
+//       console.log("[Middleware] next state", store.getState());
+//       return result;
+//     };
+//   };
+// };
+
+const logger = store => next => action => {
+  console.log("[Middleware Dispatching]", action);
+  const result = next(action);
+  console.log("[Middleware] next state", store.getState());
+  return result;
+};
+
 const store = createStore(
   reducer,
+  applyMiddleware(logger),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
