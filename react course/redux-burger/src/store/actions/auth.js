e120b02eafd6acce2e5fcfbd1abeb26c891a341f
Tuanpa <tuan.pa.1987@gmail.com>
2018-07-09 18:27:49 +0700

    update

--- react course/redux-burger/src/store/actions/auth.js
+++ react course/redux-burger/src/store/actions/auth.js
@@ -16,6 +16,14 @@ const authFail = err => ({
   error: err
 });
 
+const logout = () => ({
+  type: actionTypes.AUTH_LOGOUT
+});
+
+const checkAuthTimeout = expirationTime => dispatch => {
+  setTimeout(() => dispatch(logout()), expirationTime * 1000);
+};
+
 export const auth = (email, password, isSignup) => dispatch => {
   dispatch(authStart());
   const authData = {
@@ -35,6 +43,7 @@ export const auth = (email, password, isSignup) => dispatch => {
     .then(response => {
       console.log(response);
       dispatch(authSuccess(response.data.idToken, response.data.localId));
+      dispatch(checkAuthTimeout(response.data.expiresIn));
     })
     .catch(err => {
       console.log(err.response);
