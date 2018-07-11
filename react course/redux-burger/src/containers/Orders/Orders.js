de5826c5d7e61265accefa69e7d7e77536e96dac
tuanpa87 <tuanpa87@users.noreply.github.com>
2018-07-12 00:20:07 +0700

    update

--- react course/redux-burger/src/containers/Orders/Orders.js
+++ react course/redux-burger/src/containers/Orders/Orders.js
@@ -8,7 +8,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders(this.props.token);
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
 
   render() {
@@ -34,11 +34,12 @@ class Orders extends Component {
 const mapStateToProps = state => ({
   orders: state.order.orders,
   loading: state.order.loading,
-  token: state.auth.token
+  token: state.auth.token,
+  userId: state.auth.userId
 });
 
 const mapDispatchToProps = dispatch => ({
-  onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
+  onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
 });
 
 export default connect(
--- react course/redux-burger/src/store/actions/order.js
+++ react course/redux-burger/src/store/actions/order.js
@@ -49,10 +49,12 @@ const fetchOrderStart = () => ({
   type: actionTypes.FETCH_ORDER_START
 });
 
-export const fetchOrders = token => dispatch => {
+export const fetchOrders = (token, userId) => dispatch => {
   dispatch(fetchOrderStart());
+  const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`
+  console.log(queryParams)
   axios
-    .get("/orders.json?auth=" + token)
+    .get("/orders.json" + queryParams)
     .then(res => {
       const fetchedOrders = [];
       for (let key in res.data) {
