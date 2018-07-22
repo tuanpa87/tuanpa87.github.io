0c1b9aeccda449141c6c6804ad2660ffed33b702
tuanpa87 <tuanpa87@users.noreply.github.com>
2018-07-23 00:25:47 +0700

    update

--- react course/react-animation/src/App.js
+++ react course/react-animation/src/App.js
@@ -37,6 +37,12 @@ class App extends Component {
           timeout={1000}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
         >
           {state => (
             <div>
@@ -55,14 +61,7 @@ class App extends Component {
           )}
         </Transition>
         <br /> <br /> <br />
-        <Transition
-          in={this.state.modalIsOpen}
-          timeout={1000}
-          mountOnEnter
-          unmountOnExit
-        >
-          {state => <Modal show={state} closed={this.closeModal} />}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.openModal}>
           Open Modal
